6682f78c85a6164e299f2ab33cb4ecca54467421
Barney Moss <barneym@fb.com>
2021-12-07 08:35:05 +0000

    day 7, getting harder!

---
+++ day7/aoc.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3.8
+# https://adventofcode.com/2021/day/7
+
+from sys import argv
+
+def costSimple(target, positions): # q1
+    return sum([abs(pos - target) for pos in positions])
+
+cache = {}
+def costComplex(target, positions): # q2
+    cost = 0
+    for pos in positions:
+        if (target, pos) in cache:
+            cost += cache[(target, pos)]
+            continue
+        thisCost = sum(range(abs(pos - target)+1))
+        cost += thisCost
+        cache[(target, pos)] = thisCost
+    return cost
+
+def q1(positions):
+    positions = [int(s) for s in positions[0].split(',')]
+    best = positions[0]
+    bestCost = costComplex(best, positions)
+    for target in range(min(positions) + 1, max(positions) + 1): # binary search might work, and would be much faster
+        thisCost = costComplex(target, positions)
+        if thisCost < bestCost:
+            bestCost = thisCost
+            best = target
+    print(f'{best=} {bestCost=}')
+    return bestCost
+
+if len(argv) > 1:
+    with open(argv[1], 'r') as f:
+        print(q1([l.strip() for l in f]))
+else:
+    print(q1([l.strip() for l in '''16,1,2,0,4,2,7,1,2,14'''.splitlines()]))
